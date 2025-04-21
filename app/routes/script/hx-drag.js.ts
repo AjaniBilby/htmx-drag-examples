f8fc8f3269aff32ed8fd5653f4cf64a7962d2dd7
Ajani Bilby <me@ajanibilby.com>
2025-04-21 12:11:32 +1000

    ~moved folders

--- app/routes/script/hx-drag.js.ts
+++ app/routes/script/hx-drag.js.ts
@@ -5,6 +5,6 @@ import { readFile } from "fs/promises";
 export async function loader({ headers }: RouteContext) {
 	headers.set("Content-Type", "text/javascript");
 
-	const file = await readFile("./hx-drag.js");
+	const file = await readFile("./src/hx-drag.js");
 	return new Response(file, { headers });
 }
